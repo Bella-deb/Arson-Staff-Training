4463d25c97dc7541f3aaac8375c24b542916ebd4
Bella <bellazr@proton.me>
2024-02-20 15:36:55 -0500

    changes

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,13 @@
-// Ready event for bot startup.
+// Make Ready Event Text Look Pretty
+const { blue, bold, underline } = require("colorette");
 
+// Ready event for bot startup.
 module.exports = (client) => {
   // Should Look Something Like:
   // Ready to serve in 14 channels on 2 servers, for a total of 14 users.
   // Logged in as Arson Staff Training#6419 || Bot ID: 1209283589519446127
   console.log(
-    `Logged in as ${client.user.tag} || Bot ID: ${client.user.id}\nReady to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
+    `🚀 Logged in as ${bold(client.user.tag)} || Bot ID: ${client.user.id}\n🚀 Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
   );
 
   // Makes activity: "Watching staff members!"
--- index.js
+++ index.js
@@ -29,59 +29,29 @@ const client = new Client({
     GatewayIntentBits.DirectMessages,
 });
 
+// Make config available everywhere
+client.config = config;
+
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection with the key as the command name and the value as the exported module
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
-}
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!' });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!' });
-		}
-	}
-});
-
 const events = fs
-  .readdirSync("./events")
+  .readdirSync('./events')
   .filter((file) => file.endsWith(".js"));
 for (const file of events) {
   const eventName = file.split(".")[0];
   const event = require(`./events/${file}`);
   client.on(eventName, event.bind(null, client));
+}
+
+const commands = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith(".js"));
+for (const file of commands) {
+  const commandName = file.split(".")[0];
+  const command = require(`./commands/${file}`);
+
+  console.log(`Attempting to load command ${commandName}`);
+  client.commands.set(commandName, command, client);
 }
 
 client.login(config.token);
