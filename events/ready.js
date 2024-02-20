73034fb46b20c7e346f4178826bd599ec8dfa965
Bella <bellazr@proton.me>
2024-02-20 11:59:16 -0500

    add activity type

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
 module.exports = (client) => {
-    console.log(
-      `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
-    );
-    console.log(`Logged in as ${client.user.tag} || Bot ID: ${client.user.id}`);
-    client.user.setPresence({
-      activities: [{ name: "staff members!", type: "WATCHING" }],
-      status: "online",
-    });
+  console.log(
+    `Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`
+  );
+  console.log(`Logged in as ${client.user.tag} || Bot ID: ${client.user.id}`);
+  client.user.setPresence({
+    activities: [{ type: 3, name: "staff members!" }],
+    status: "online",
+  });
 };
