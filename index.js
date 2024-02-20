6fcb0b40c5cff42df2547718bafc91a442b667e6
Bella <bellazr@proton.me>
2024-02-20 11:35:20 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const fs = require("fs");
 const config = require('./config.json');
+
+const client = new Client({
+    messageCacheLifetime: 60,
+    fetchAllMembers: true,
+    messageCacheMaxSize: 10,
+    restTimeOffset: 0,
+    restWsBridgetimeout: 100,
+    shards: "auto",
+    allowedMentions: {
+      disableMentions: true,
+      repliedUser: true,
+    },
+    partials: [
+      "MESSAGE",
+      "CHANNEL",
+      "REACTION",
+      "GUILD_MEMBER",
+      "USER",
+      "VOICE",
+      "DIRECT_MESSAGES",
+    ],
+    intents:
+      3276799 |
+      GatewayIntentBits.MessageContent |
+      GatewayIntentBits.DirectMessages,
+  });
+
+  client.login(config.token);
