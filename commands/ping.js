7e30e0ce46177b1428b062cf9e9ef8658db179a5
Bella <bellazr@proton.me>
2024-02-20 21:47:59 -0500

    add

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,9 @@
+const { bold } = require("colorette");
+
 exports.run = (client, message, args) => {
   message.channel.send("Pong!").catch(console.error);
+
+  console.log(`${bold('Ping Command Used:')}\nUser: ${message.author.tag}\nUser ID: ${message.author.id}`)
 };
 
 exports.name = "ping";
